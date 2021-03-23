31579b1b49feb5720f06168b0e120136c6b592ab
Diego Enrique Leon Ortiz <diego.leon1@unmsm.edu.pe>
2021-03-23 18:25:49 -0500

    Prueba push

--- categorias.php
+++ categorias.php
@@ -8,12 +8,12 @@ include('functioncategorias.php');
     <div class="container-fluid">
         <div class="row mb-2">
             <div class="col-sm-6">
-                <h3 class="page-header"><i class="fa fa-table"></i> Categorías PULL REQUEST  - COLABORADOR (JA)</h3>
+                <h3 class="page-header"><i class="fa fa-table"></i> Categorías PULL REQUEST</h3>
             </div><!-- /.col -->
             <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
                     <li class="breadcrumb-item"><a href="index.php">Inicio</a></li>
-                    <li class="breadcrumb-item active">Tabla</li>
+                    <li class="breadcrumb-item active">Tabla - Colaborador:DiegoLeon</li>
                     <li class="breadcrumb-item active">Categorías</li>
                 </ol>
             </div><!-- /.col -->
